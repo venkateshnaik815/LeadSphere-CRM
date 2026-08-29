bdf8bac17275d2870b29b4fc8213646fe21d98b4
Venkatesh <venkateshnaik.gov@gmail.com>
2026-08-29 14:20:57 +0530

    chore: initialize spring boot backend and react frontend

---
+++ backend/src/test/java/com/leadsphere/crm/LeadSphereCrmApplicationTests.java
@@ -0,0 +1,13 @@
+package com.leadsphere.crm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LeadSphereCrmApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
